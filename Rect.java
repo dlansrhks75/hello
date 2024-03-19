2e1f15a6ba03c496a90f4aa71405fc67a169a86e
dlansrhks75 <dlansrhks75@naver.com>
2024-03-19 10:17:25 +0900

    setter, getter 추가

--- Rect.java
+++ Rect.java
@@ -1,6 +1,23 @@
 class Rect{
 	int width;
 	int height;
+
+	public void setWidth(String width){
+		this.width = width;
+	}
+
+	public void setHeight(int height){
+		this.height = height;
+	}
+
+	public String getWidth(){
+		return name;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
 	public int getArea(){
 		return width*height;
 	}
